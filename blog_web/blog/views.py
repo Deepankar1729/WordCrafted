7cae4d05114c5a7f9f4e726ea7a86d7c34c482a3
Deepankar <187712367+Deepankar1729@users.noreply.github.com>
2026-03-10 09:24:33 +0530

    limit normal blogs

--- blog_web/blog/views.py
+++ blog_web/blog/views.py
@@ -12,7 +12,7 @@ from django.views.decorators.cache import never_cache
 def home(request):
     categories = Category.objects.all()
     featured_blogs = Blog.objects.filter(is_featured = True, status = 'Published').order_by('updated_at')
-    normal_blogs = Blog.objects.filter(is_featured = False, status = 'Published')
+    normal_blogs = Blog.objects.filter(is_featured = False, status = 'Published').order_by('-created_at')[:3]
 
     # fetching about
     try:
